d389a0dc824f9fb5b1f083aa8bfb68dd1f0fe34b
SUSMITHAdindi <86483106+SUSMITHAdindi@users.noreply.github.com>
2021-06-30 19:47:33 +0530

    Add files via upload

---
+++ HarshadNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class HarshadNumber
+{
+public static void main(String args[])
+{
+int n,r,s=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int num=n;
+while(n>0)
+{
+r=n%10;
+s+=r;
+n/=10;
+}
+if(num%s==0)
+System.out.println(num+" is a harshad number");
+else
+System.out.println(num+" is a not harshad number");
+}
+}
