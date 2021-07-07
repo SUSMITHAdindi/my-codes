6ee09e8e15fb8f67ffaf1ffcab9b38960a2e5563
SUSMITHAdindi <86483106+SUSMITHAdindi@users.noreply.github.com>
2021-07-08 00:19:24 +0530

    Update PrimeArray.java

--- PrimeArray.java
+++ PrimeArray.java
@@ -9,7 +9,7 @@ class PrimeArray
 	int a[]=new int[n];
 	for(int i=0;i<n;i++)
 	a[i]=sc.nextInt();
-	int c=0;
+	int c=0,c1=0;
         for(int i=0;i<n;i++)
 	{
 		if(a[i]>1)
@@ -18,17 +18,20 @@ class PrimeArray
 		for(int j=2;j<=a[i]/2;j++)
 		{
 		if(a[i]%j==0)
+        	{
         	c=-1;
+		c1++;
+		break;
+		}
 		}
 		if(c==0)
         	System.out.print(a[i]+" ");
-		
 		}
+		else
+		c1++;
 	}
-	if(c==-1)
-        {
-	System.out.println("there are no prime numbers in the array");
-        }
+	if(c1==n)
+	System.out.println("there are no prime numbers");
 	}
 }
 ---------------------------------------------------------------------------------
@@ -39,5 +42,5 @@ C:\susi>java PrimeArray
 2 3 5              //these are the prime number in the array
 C:\susi>java PrimeArray
 5
-1 4 6 8 0
-there are no prime numbers in the array
+1 4 -5 6 8
+there are no prime numbers
